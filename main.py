ec0eb773dda388c8ed622481cf0a9472dcb45cd4
ifukin <57060355+ifukin@users.noreply.github.com>
2020-11-13 17:09:29 +0300

    My 3 Commit
    Кто-то, возможно я сам, внес изменения...

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def print_hi(name):
     print(f'Hi, {name}')
     print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 if __name__ == '__main__':
     print_hi('PyCharm')
